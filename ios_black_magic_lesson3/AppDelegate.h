2a66c17a1d0c47f872af1e5207172ac077efc878
WilliamZang <chengwei.zang.1985@gmail.com>
2017-03-10 01:20:21 +0800

    Initial Commit

---
+++ ios_black_magic_lesson3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios_black_magic_lesson3
+//
+//  Created by Chengwei Zang on 2017/3/10.
+//  Copyright © 2017年 Chengwei Zang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios_black_magic_lesson3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios_black_magic_lesson3
+//
+//  Created by Chengwei Zang on 2017/3/10.
+//  Copyright © 2017年 Chengwei Zang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
